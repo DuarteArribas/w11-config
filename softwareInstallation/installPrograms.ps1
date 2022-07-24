99842da7222b3b5f6c7e4898eb17caf9043beec8
YozNacks <duarte.a.arribas@gmail.com>
2022-07-24 21:11:05 +0100

    added chocolatey installation to script of software installation

--- softwareInstallation/installPrograms.ps1
+++ softwareInstallation/installPrograms.ps1
@@ -1,3 +1,12 @@
+# Allow this script to run
+Set-ExecutionPolicy Bypass -Scope Process
+
+# Install chocolatey if it is not yet installed
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Upgrade chocolatey
+choco upgrade -y chocolatey
+
 # Loops through the contents of 'packages.config'
 foreach($line in Get-Content .\packages.config){
   $packages += $line + " "
