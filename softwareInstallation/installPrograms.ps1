3f6dafb00415cac9e9133a620083e86b3d3a60a5
YozNacks <duarte.a.arribas@gmail.com>
2022-07-27 22:22:38 +0100

    fixed a bug where the office wasn't being installed when ran on an administrator prompt

--- softwareInstallation/installPrograms.ps1
+++ softwareInstallation/installPrograms.ps1
@@ -1,3 +1,27 @@
+# == Install office ==
+# Check if the Office installation directory
+# already exists and delete it if so
+if(Test-Path -Path "Office"){
+  Remove-Item Office -Recurse
+}
+# Install the corresponding office version (64 bit
+# for 64 bit machines and 32 bit for 32 bit machines)
+if([System.Environment]::Is64BitOperatingSystem){
+  Write-Output("Installing office 64bit...")
+  .\ms-office\setup.exe /download ms-office\configuration-Office365-x64.xml
+  .\ms-office\setup.exe /configure ms-office\configuration-Office365-x64.xml
+  Write-Output("Finished office 64bit installation")
+}
+else{
+  Write-Output("Installing office 32bit...")
+  .\ms-office\setup.exe /download ms-office\configuration-Office365-x86.xml
+  .\ms-office\setup.exe /configure ms-office\configuration-Office365-x86.xml
+  Write-Output("Finished office 32bit installation")
+}
+# Remove residue of the office installation directory
+Remove-Item Office -Recurse
+
+# == Install other applications ==
 # Rerun this script as administrator if it is not already
 if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){
   Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
@@ -11,7 +35,6 @@ foreach($path in Write-Output($PSCommandPath -split "\\")){
   }
 }
 $packagesPath = $softwareInstallation + "packages.config"
-$officePath   = $softwareInstallation + "\ms-office"
 
 # Install chocolatey if it is not yet installed
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -32,16 +55,3 @@ foreach($line in Get-Content $packagesPath){
 
 # Upgrade all packages
 choco upgrade all
-
-# Install office
-if([System.Environment]::Is64BitOperatingSystem){
-  Write-Output("Installing office 64bit...")
-  .\$officePath\setup.exe /download configuration-Office365-x64.xml
-  .\$officePath\setup.exe /configure configuration-Office365-x64.xml 
-}
-else{
-  Write-Output("Installing office 32bit...")
-  .\$officePath\setup.exe /download configuration-Office365-x86.xml
-  .\$officePath\setup.exe /configure configuration-Office365-x86.xml
-}
-Remove-Item "$officePath\office" -Recurse
