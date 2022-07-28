be2bfe54fe726688938827f64f8e54eca80dae5e
YozNacks <duarte.a.arribas@gmail.com>
2022-07-28 18:39:54 +0100

    added output printing for rerunnin the script as administrator on update programs

--- softwareInstallation/updatePrograms.ps1
+++ softwareInstallation/updatePrograms.ps1
@@ -1,5 +1,6 @@
 # Rerun this script as administrator if it is not already
 if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){
+  Write-Output("This shell does not have administrative privileges. Rerunning in administrative mode...")
   Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
   Exit
 }
