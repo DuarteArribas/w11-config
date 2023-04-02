c9f0fb342df97f67e3340e4313892fba35a2f518
DuarteArribas <duarte.a.arribas@gmail.com>
2023-04-02 04:39:21 +0100

    added correct inconfig

--- wslConfig/inconfig.sh
+++ wslConfig/inconfig.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 echo "Setting initial configurations..."
-echo "Copying files..."
-sudo cp .gitconfig user/.bashrc ~
-echo "Files successfully copied!"
 echo "Installing default packages..."
-sudo apt update && sudo apt upgrade
-sudo apt install git-all
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-sudo apt install net-tools
-sudo apt install nasm
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install postgresql
-sudo apt install openvpn
-curl -sS https://starship.rs/install.sh | sh
-sudo apt update && sudo apt upgrade
+sudo apt -y update && sudo apt -y upgrade
+sudo apt -y install git-all
+sudo apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt -y install net-tools
+sudo apt -y install nasm
+sudo apt -y install python3
+sudo apt -y install python3-pip
+sudo apt -y install postgresql
+sudo apt -y install openvpn
+curl -y -sS https://starship.rs/install.sh | sh
+sudo apt -y update && sudo apt upgrade
 echo "Packages successfully installed!"
+echo "Copying files..."
+sudo cp .gitconfig .bashrc ~
+sudo mkdir ~/.config
+sudo cp starship.toml ~/.config
+echo "Files successfully copied!"
